d20befee7dad55fd342ba7429cf7623b9cec1b9a
Hack88id <107460953+Hack88id@users.noreply.github.com>
2022-06-14 13:53:18 +0700

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,21 @@
+import os
+import time
+
+os.system("clear")
+os.system('figlet ADMIN')
+
+print("""
+
+
+
+""")
+
+gc = input("masukan link grup ~> ")
+no = input ("masukan nomor anda contoh +628×× ~> ")
+
+print ("proses hack")
+
+p = 99999999999999999999999999999999999999999999999
+while True:
+	for i in range(999999999999999999, p):
+		print(i)
